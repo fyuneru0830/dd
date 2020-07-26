b37772f17989b36294e189c64cbc52b4a62c71bf
fyuneru0830 <36604859+fyuneru0830@users.noreply.github.com>
2020-07-27 00:40:08 +0900

    Delete InstallSimple.sh

--- InstallSimple.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-bash <(wget --no-check-certificate -qO- 'https://moeclub.org/attachment/LinuxShell/InstallNET.sh') -dd "https://image.moeclub.org/GoogleDrive/1OVA3t-ZI2arkM4E4gKvofcBN9aoVdneh"
